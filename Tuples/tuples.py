37b977aebee21038392ad2bb38d27d701c472984
MrRutledge <rahimrutledge@gmail.com>
2019-07-28 23:43:30 +0100

    Twitter API Usage

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -46,6 +46,17 @@
 # for key, val in lst[:10]:
 #       print(key, val)
 
+age = - 1
+while age <= 0:
+        try:
+                age = int(input('Enter your age in years'))
+                if age <= 0:
+                        print('Your age must be positive')
+        except ValueError:
+                        print('That is an invalid age specification')
+        except EOFError:
+                print('There was an unexcepted error reading input.')
+                raise   #let's re-raise this exception 
 
 # fh  = open('../Data/mbox_short.txt')
 
