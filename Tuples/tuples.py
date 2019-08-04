ed25f4e90f9c67cf2727440fb1d7441dcf109c67
MrRutledge <rahimrutledge@gmail.com>
2019-08-04 23:45:03 +0100

    Twitter API Usage

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -21,30 +21,30 @@
 # t
 
 
-# t.sort()
-# t
-# for key, val in list(d.items()):
-#     print(val, key)
+ t.sort()
+ t
+ for key, val in list(d.items()):
+     print(val, key)
 
-# import string
-# fhand = open('romeo-full.txt')
-# counts = dict()
-# for line in fhand:
-#       line = line.translate(str.maketrans('', '', string.punctuation))
-#       line = line.lower()
-#       words = line.split()
-# for word in words:
-#       if word not in counts:
-#               counts[word] = 1
-#       else:
-#               counts[word] += 1
-# # Sort the dictionary by value
-# lst = list()
-# for key, val in list(counts.items()):
-#       lst.append((val, key))
-#       lst.sort(reverse=True)
-# for key, val in lst[:10]:
-#       print(key, val)
+ import string
+ fhand = open('romeo-full.txt')
+ counts = dict()
+ for line in fhand:
+       line = line.translate(str.maketrans('', '', string.punctuation))
+       line = line.lower()
+       words = line.split()
+ for word in words:
+       if word not in counts:
+               counts[word] = 1
+       else:
+               counts[word] += 1
+                # Sort the dictionary by value
+ lst = list()
+ for key, val in list(counts.items()):
+       lst.append((val, key))
+       lst.sort(reverse=True)
+ for key, val in lst[:10]:
+       print(key, val)
 
 age = - 1
 while age <= 0:
